50bc19eae7281ec8b2a05ef7a1fe43f2a5e1681b
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-08-02 17:06:52 +0900

    user

---
+++ src/main/java/com/yu/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.yu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.yu.model.User;
+
+@Controller
+public class UserController {
+
+	private List<User> users = new ArrayList<User>();
+	
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user);
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users", users);
+		return "list";
+	}
+	
+}
